f56a0de0add8d6d4df93ec206e5d24df4422a15e
yangja057 <yangja057@gmail.com>
2018-06-02 18:36:16 +0900

    SetStepActivity 생성

---
+++ app/src/main/java/com/example/newwayforblind/SetStepActivity.java
@@ -0,0 +1,13 @@
+package com.example.newwayforblind;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SetStepActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_set_step);
+    }
+}
