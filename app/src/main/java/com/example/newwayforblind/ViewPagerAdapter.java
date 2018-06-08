1302ca34e2bf3552f5ccc900844c403de4f5bc28
Yoon <yoonsb94@gmail.com>
2018-06-08 13:56:15 +0900

    MainActivity 전면수정 / BottomNavigationView / ViewPager / ViewPagerAdapter / selector.xml / menu.xml / Fragment 3개

---
+++ app/src/main/java/com/example/newwayforblind/ViewPagerAdapter.java
@@ -0,0 +1,30 @@
+package com.example.newwayforblind;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+import java.util.ArrayList;
+
+public class ViewPagerAdapter extends FragmentPagerAdapter {
+
+    final ArrayList<Fragment> mFragmentList = new ArrayList<>();
+
+    public ViewPagerAdapter(FragmentManager fm) {
+        super(fm);
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        return mFragmentList.get(position);
+    }
+
+    @Override
+    public int getCount() {
+        return mFragmentList.size();
+    }
+
+    public void addFragment(Fragment fragment) {
+        mFragmentList.add(fragment);
+    }
+}
