ae8627138c1d05849ab2b613c8dbad2a00d6c785
Yeori <yoeri1123@naver.com>
2018-06-08 12:50:06 +0900

    orientationTestCommit

--- app/src/main/java/com/example/newwayforblind/OrientationTestActivity.java
+++ app/src/main/java/com/example/newwayforblind/OrientationTestActivity.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class OrientationTestActivity extends AppCompatActivity {
 
@@ -16,6 +17,14 @@ public class OrientationTestActivity extends AppCompatActivity {
 
     private TextView step;
     private TextView orientation;
+    private TextView direction;
+    int liveCoount=0;
+    int warningCount=0;
+
+    final int THRESHOLD=90;
+    final int CORRECTION=360;
+
+    int last=0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +33,7 @@ public class OrientationTestActivity extends AppCompatActivity {
 
         step = (TextView) findViewById(R.id.step);
         orientation = (TextView) findViewById(R.id.orientation);
+        direction = (TextView) findViewById(R.id.direction);
 
         mOrientation = new Orientation(getApplicationContext());
         mHandler = new Handler(){
@@ -33,11 +43,101 @@ public class OrientationTestActivity extends AppCompatActivity {
                 Log.d("Handler", "");
                 step.setText(stepCheck.getStep() + "");
                 orientation.setText(mOrientation.getOrientation() + "");
+                checkHappiness(mOrientation.getOrientation());
             }
         };
         stepCheck = new StepCheck(getApplicationContext(), mHandler);
     }
 
+        public void checkHappiness(int ori){
+
+            int b=last-THRESHOLD;
+            int a=last+THRESHOLD;
+
+            /*
+            * 보정작업
+            * min이 음수가 되면 360을 더하고,
+            * max가 양수가 되면 360을 뺀다.*/
+
+            if(a>360){ // 4사분면
+                if((b<=ori&&ori<=360) || (0<=ori&&ori<=(a-CORRECTION))){ // 방향을 전환하지 않음
+                    direction.setText("직진");
+                }else{ // 방향을 전환 함
+                    if(last - 180 <= ori && ori < last){
+                        // 왼쪽
+                    }else if((0 <= ori && ori < last - 180) || (last <= ori && ori < 360)){
+                        // 오른쪽
+                    }
+                }
+            }else if(b<0){ // 1사분면
+                if((0<=ori&&ori<=a) || ((b+CORRECTION)<=ori&&ori<=360)){ // 방향을 전환하지 않음
+                    direction.setText("직진");
+                }
+                else{ // 방향을 전환 함
+                    if(last <= ori && ori < last - 180){
+                        // 오른쪽
+                    }else if((last - 180 <= ori && ori < 360) || (0 <= ori && ori < last)){
+                        // 왼쪽
+                    }
+                }
+            }else if(90 <= last && last < 180){ //2사분면
+                if(b<=ori && ori<=a){ // 방향을 전환하지 않음
+                    direction.setText("직진");
+                }else{ // 방향을 전환 함
+                    if(last <= ori && ori < last + 180){
+                        // 오른쪽
+                    }else if((0 <= ori && ori < last) || (last + 180 <= ori && ori < 360)){
+                        // 왼쪽
+                    }
+                }
+            }else if(180 <= last && last < 270){ // 3사분면
+                if(b<=ori && ori<=a){ // 방향을 전환하지 않음
+                    direction.setText("직진");
+                }else{ // 방향을 전환 함
+                    if((last <= ori && ori < 360) || (0 <= ori && ori < last - 180)){
+                        // 오른쪽
+                    }else if(last - 180 <= ori && ori < last){
+                        // 왼쪽
+                    }
+                }
+            }else if(270<=last && last <360){//4사분면
+                if(b<=ori && ori<=a){
+                    //방향 전환 하지 않음
+                    direction.setText("직진");
+                }else{
+                    //방향을 전환 함
+                    if (last-180<=ori && ori<last){
+                        //왼쪽
+                    }else if((last<=ori && ori<360) || (0<=ori && ori<last-180)){
+                       //오른쪽
+                    }
+                }
+            }else{
+                Toast.makeText(this, "속해있지 않은 범위값 쥬륵 다시짜세요", Toast.LENGTH_SHORT).show();
+            }
+
+            if((min)<=ori&&ori<=(max)){
+                //범위 안에 있을 때는 행복한 값이야.
+                last=ori;
+                warningCount=0;
+            }else{
+
+                warningCount++;
+
+                if(warningCount>3){
+                    if((last-ori)<0){
+                        Toast.makeText(this, "오른쪽", Toast.LENGTH_SHORT).show();
+                        warningCount=0;
+                    }
+                    else{
+                        Toast.makeText(this, "왼쪽", Toast.LENGTH_SHORT).show();
+                        warningCount=0;
+                     }
+                }
+
+            }
+
+        }
     public void onStart(View view) {
         stepCheck.startSensor();
         mOrientation.startSensor();
